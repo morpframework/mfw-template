8bf1d1edcaa44318d8f6203a89496b3092801e20
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-12-09 12:26:09 +0800

    use automatically generated sqlalchemy model by default

--- mfw_template/templates/resource/{{cookiecutter.module_name}}/storage.py
+++ mfw_template/templates/resource/{{cookiecutter.module_name}}/storage.py
@@ -1,5 +1,6 @@
 import morpfw
 import morpfw.sql
+from morpfw.sql import construct_orm_model
 import sqlalchemy as sa
 import sqlalchemy_utils as sautils
 import sqlalchemy_jsonfield as sajson
@@ -7,18 +8,15 @@ import sqlalchemy_jsonfield as sajson
 from ..app import App
 from .model import {{cookiecutter.type_name}}Model
 
-class {{cookiecutter.type_name}}(morpfw.sql.Base):
-
-    __tablename__ = '{{cookiecutter.project_name}}_{{cookiecutter.module_name}}'
-
-    title = sa.Column(sa.String(length=1024))
-    description = sa.Column(sa.Text())
-
-
 class {{cookiecutter.type_name}}Storage(morpfw.SQLStorage):
-    model = {{cookiecutter.type_name}}Model
-    orm_model = {{cookiecutter.type_name}}
 
+    model = {{cookiecutter.type_name}}Model
+
+    orm_model = construct_orm_model(
+            schema={{cookiecutter.type_name}}Model.schema,
+            metadata=morpfw.sql.Base.metadata,
+            name='{{cookiecutter.project_name}}_{{cookiecutter.module_name}}'
+    )
 
 @App.storage(model={{cookiecutter.type_name}}Model)
 def get_storage(model, request, blobstorage):
