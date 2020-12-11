13707849589df7bd128c0175f2971c1efa3b6bed
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-12-11 21:51:52 +0800

    added implementation hints for validators

--- mfw_template/templates/fieldvalidator/{{ cookiecutter.module_container }}/{{cookiecutter.module_name}}.py
+++ mfw_template/templates/fieldvalidator/{{ cookiecutter.module_container }}/{{cookiecutter.module_name}}.py
@@ -1,3 +1,10 @@
-def validate(request, schema, field, value, mode=None):
-    """ Validate data """
-    return True
+import typing
+
+def validate(request, schema, field, value, mode=None) -> typing.Optional[str]:
+    """ Validate data. 
+
+    Returns error message if fail.
+    Returns None if no errors
+
+    """
+    return 
